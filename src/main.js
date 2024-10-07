e51ab892ba60d733a3d69bdb320763b9452b4016
andreimelo <andreiagno.melo@gmail.com>
2024-10-06 23:08:45 -0500

    setup: router view

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+// Create the Vue app instance
+const app = createApp(App);
+
+// Use the router with the app
+app.use(router);
+
+// Mount the app to the DOM element with id #app
+app.mount('#app');
